1160e7d16cabebc0cd5355d6fdcc74e315797ff1
Anil Khatiwada <anilkhatiwada@gmail.com>
2022-06-18 23:19:24 +0545

    from action

--- pages/api/user.ts
+++ pages/api/user.ts
@@ -2,11 +2,22 @@ import excuteQuery from '../../lib/db'
 
 export default async (req, res) => {
     if (req.method === 'POST') {
-        excuteQuery(`INSERT INTO user (username, full_name, email, password) VALUES ('${req.body['username']}', '${req.body['full_name']}', '${req.body['email']}', '${req.body['password']}')`, [])
-        let user = await excuteQuery('SELECT username, full_name FROM user', [])
-        res.send(user);
-    } else {
-        let user = await excuteQuery('SELECT username, full_name FROM user', [])
-        res.send(user);
-    }
+        excuteQuery(`INSERT INTO user (username, full_name, email, password) VALUES ('${req.body['username']}', '${req.body['full_name']}', '${req.body['email']}', '${req.body['password']}')`, []);
+        let user = await excuteQuery('SELECT username, full_name FROM user', []);
+        res.send({
+            {
+                'success': true,
+                'message': "registration successful",
+                'data': user
+            }
+        });
+} else {
+    let user = await excuteQuery('SELECT username, full_name FROM user', [])
+    res.send(
+        {
+            'success': true,
+            'data': user
+        }
+    );
+}
 };
