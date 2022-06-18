0591306469564be7e958dadb94ced674339998f3
Anil Khatiwada <anilkhatiwada@gmail.com>
2022-06-18 23:06:49 +0545

    database

--- pages/api/user.ts
+++ pages/api/user.ts
@@ -1,6 +1,12 @@
 import excuteQuery from '../../lib/db'
 
 export default async (req, res) => {
-    let user = await excuteQuery('SELECT username, full_name FROM user', [])
-    res.send(user);
+    if (req.method === 'POST') {
+        excuteQuery(`INSERT INTO user (username, full_name, email, password) VALUES ('${req.body['username']}', '${req.body['full_name']}', '${req.body['email']}', '${req.body['password']}')`, [])
+        let user = await excuteQuery('SELECT username, full_name FROM user', [])
+        res.send(user);
+    } else {
+        let user = await excuteQuery('SELECT username, full_name FROM user', [])
+        res.send(user);
+    }
 };
